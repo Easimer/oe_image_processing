b0d309bb85530b1ec4dc18de3683ae9b1a7820e6
Daniel Meszaros <easimer@gmail.com>
2020-11-13 13:22:00 +0100

    Finish the subtitle detection code

--- as/oeip.cpp
+++ as/oeip.cpp
@@ -6,6 +6,18 @@
 // TODO: This should be configurable by the user
 static constexpr int total_edge_buffers = 4;
 
+#define UMat Mat
+
+struct Edge_Buffers {
+	Edge_Buffers(int n, cv::Size const& size) {
+		for (int i = 0; i < n; i++) {
+			_buffers.emplace_back(cv::UMat(size, CV_16U));
+		}
+	}
+
+	std::vector<cv::UMat> _buffers;
+};
+
 static int get_edge_buffer_weight(int c, int n, int i) {
 	// i = c -> n
 	// else (i + n - c) `mod` n
@@ -58,7 +70,7 @@ public:
 
 		for (int i = 0; i < total_edge_buffers; i++) {
 			cv::Mat tmp;
-			_edge_buffers[i] = cv::UMat(height, width, CV_16S);
+			_edge_buffers[i] = cv::UMat::zeros(height, width, CV_8U);
 		}
 	}
 
@@ -71,9 +83,29 @@ protected:
 		_cb_benchmark = fun;
 	}
 
+	bool _init = false;
+
+	void edge_buffers_average(cv::UMat& avg, cv::UMat& avg_bin, cv::Size const& size) {
+		// NOTE: maguk az edge bufferek 8U formatumban vannak es 0-255 normalizalt ertekek vannak benne
+		// Ezeket akkumulaljuk 16U matrixba
+		cv::UMat acc(size, CV_16U, cv::Scalar(0));
+		for (int i = 0; i < total_edge_buffers; i++) {
+			// Sulyozott atlag; a legujabb kapja a legnagyobb sulyt, a legregebbi a legkisebbet
+			cv::UMat eb(size, CV_16U);
+			_edge_buffers[i].convertTo(eb, CV_16U);
+			auto w = get_edge_buffer_weight(_edge_buffers_cursor, total_edge_buffers, i);
+			acc += w * eb;
+		}
+
+		auto const sum_of_weights = total_edge_buffers * (total_edge_buffers + 1) / 2;
+		cv::UMat avg_tmp = acc / sum_of_weights;
+
+		avg_tmp.convertTo(avg, CV_8U);
+		threshold(avg, avg_bin, 252, 255, cv::THRESH_BINARY);
+	}
+
 	bool step_impl() {
 		cv::Mat buf;
-		cv::UMat edge_cur;
 		bool has_output_callback = _cb_output != nullptr;
 		
 		_video.read(buf);
@@ -82,40 +114,30 @@ protected:
 			return false;
 		}
 
+		if (!_init) {
+			auto edges = detect_edges(buf);
+			for (int i = 0; i < total_edge_buffers; i++) {
+				_edge_buffers[i] = edges;
+			}
+			_init = true;
+		}
+
+
 		emit_output(OEIP_STAGE_INPUT, OEIP_COLSPACE_RGB888_RGB, buf);
 
-		// TODO: ha ezt a sort kitoroljuk, akkor nem fog mukodni az algo
-		// Nem kene a Canny()-nek kiuriteni az edge_cur matrixot?
-		edge_cur = get_edge_buffer(buf);
-		Canny(buf, edge_cur, 0, 0);
-
-		emit_output(OEIP_STAGE_CURRENT_EDGE_BUFFER, OEIP_COLSPACE_R8, edge_cur);
-
-		auto& e1 = _edge_buffers[_edge_buffers_cursor];
-
-		addWeighted(e1, 1, edge_cur, 1, 0, edge_cur, CV_16S);
+		auto edge_cur = get_edge_buffer(buf);
+		edge_cur.convertTo(edge_cur, CV_8U);
 
 		_edge_buffers_cursor = (_edge_buffers_cursor + 1) % total_edge_buffers;
 		_edge_buffers[_edge_buffers_cursor] = std::move(edge_cur);
 
-		cv::UMat acc(buf.size(), CV_32F, cv::Scalar(0));
-		for (int i = 0; i < total_edge_buffers; i++) {
-			cv::Mat eb;
-			_edge_buffers[i].copyTo(eb);
-			auto w = get_edge_buffer_weight(_edge_buffers_cursor, total_edge_buffers, i);
-			accumulate(w * eb, acc);
-		}
+		auto edge_avg = average_edge_buffers();
+		edge_avg.convertTo(edge_avg, CV_8U);
+		cv::UMat edge_cur_bin;
+		cv::threshold(edge_avg, edge_cur_bin, 127, 255, CV_8U);
 
-		cv::UMat avg, avg_bin;
+		emit_output(OEIP_STAGE_ACCUMULATED_EDGE_BUFFER, OEIP_COLSPACE_R8, edge_cur_bin);
 
-		auto avg_div = total_edge_buffers * (total_edge_buffers + 1) / 2.0f;
-
-		acc.convertTo(avg, CV_8U, 1 / avg_div);
-		threshold(avg, avg_bin, 252, 255, cv::THRESH_BINARY);
-
-		subtract(_edge_buffers[_edge_buffers_cursor], avg_bin, _edge_buffers[_edge_buffers_cursor], cv::noArray(), CV_16S);		
-
-		emit_output(OEIP_STAGE_ACCUMULATED_EDGE_BUFFER, OEIP_COLSPACE_R8, avg_bin);
 
 		// megjeloljuk azokat a pixeleket, amelyek a YCbCr kep Cb es Cr csatornajanak hisztogramjaiban
 		// benne vannak a ket major bin-ben
@@ -131,48 +153,63 @@ protected:
 		buf_ycrcb_channels[1].copyTo(buf_cr);
 		buf_ycrcb_channels[2].copyTo(buf_cb);
 
-		cv::bitwise_and(buf_ycrcb_channels[1], _mask_subtitle_bottom, buf_cr);
-		cv::bitwise_and(buf_ycrcb_channels[2], _mask_subtitle_bottom, buf_cb);
+		cv::Mat mask;
+
+		cv::bitwise_and(_mask_subtitle_bottom, edge_cur_bin, mask);
+
+		cv::bitwise_and(buf_ycrcb_channels[1], mask, buf_cr);
+		cv::bitwise_and(buf_ycrcb_channels[2], mask, buf_cb);
 
 		int histSize = 256;
 		float range[] = { 0, 256 };
 		const float* histRange = { range };
 
-		cv::Mat cr_hist, cb_hist;
+		cv::UMat cr_hist, cb_hist;
 
-		// TODO: talan az acc. edge buffert kene maszkkent hasznalni amikor
-		// a ket foszint keressuk, nem pedig a textbox maszkot.
+		// Letrehozzuk a kepkocka hisztogramjat a Cr es Cb csatornakban
+		cv::calcHist(&buf_cr, 1, 0, mask, cr_hist, 1, &histSize, &histRange, true, false);
+		cv::calcHist(&buf_cb, 1, 0, mask, cb_hist, 1, &histSize, &histRange, true, false);
 
-		cv::calcHist(&buf_cr, 1, 0, _mask_subtitle_bottom, cr_hist, 1, &histSize, &histRange, true, false);
-		cv::calcHist(&buf_cb, 1, 0, _mask_subtitle_bottom, cb_hist, 1, &histSize, &histRange, true, false);
-
+		// Megkeressuk a ket foszint mindegyik csatornaban
 		int cr0, cr1;
 		int cb0, cb1;
 		two_major_bins(cr_hist, &cr0, &cr1);
 		two_major_bins(cb_hist, &cb0, &cb1);
 
-		cv::Mat thresh_cr0, thresh_cr1;
-		cv::inRange(buf_cr, cv::Scalar(cr0), cv::Scalar(cr0), thresh_cr0);
-		cv::inRange(buf_cr, cv::Scalar(cr1), cv::Scalar(cr1), thresh_cr1);
-		cv::Mat thresh_cr = cv::max(thresh_cr0, thresh_cr1);
+		cv::UMat thresh_cr0, thresh_cr1;
+		cv::inRange(buf_ycrcb_channels[1], cv::Scalar(cr0), cv::Scalar(cr0), thresh_cr0);
+		cv::inRange(buf_ycrcb_channels[1], cv::Scalar(cr1), cv::Scalar(cr1), thresh_cr1);
+		cv::UMat thresh_cr = cv::max(thresh_cr0, thresh_cr1);
 
-		cv::Mat thresh_cb0, thresh_cb1;
-		cv::inRange(buf_cb, cv::Scalar(cb0), cv::Scalar(cb0), thresh_cb0);
-		cv::inRange(buf_cb, cv::Scalar(cb1), cv::Scalar(cb1), thresh_cb1);
-		cv::Mat thresh_cb = cv::max(thresh_cb0, thresh_cb1);
+		cv::UMat thresh_cb0, thresh_cb1;
+		cv::inRange(buf_ycrcb_channels[2], cv::Scalar(cb0), cv::Scalar(cb0), thresh_cb0);
+		cv::inRange(buf_ycrcb_channels[2], cv::Scalar(cb1), cv::Scalar(cb1), thresh_cb1);
+		cv::UMat thresh_cb = cv::max(thresh_cb0, thresh_cb1);
 
-		cv::Mat thresh(thresh_cr.rows, thresh_cr.cols, CV_32F);
+		cv::UMat thresh(thresh_cr.rows, thresh_cr.cols, CV_32F);
 		thresh = cv::max(thresh_cr, thresh_cb);
 
-		cv::Mat subtitle_mask;
-		cv::Mat thresh_blur3, avg_bin_blur3;
+		cv::UMat subtitle_mask;
+		cv::UMat thresh_blur3, avg_bin_blur3;
+
+		cv::bitwise_and(edge_cur_bin, _mask_subtitle_bottom, edge_cur_bin);
+		cv::pyrDown(edge_cur_bin, edge_cur_bin, cv::Size(edge_cur_bin.cols / 2, edge_cur_bin.rows / 2));
+		cv::pyrDown(thresh, thresh, cv::Size(thresh.cols / 2, thresh.rows / 2));
 
 		cv::GaussianBlur(thresh, thresh_blur3, { 3, 3 }, 0.0f);
-		cv::GaussianBlur(avg_bin, avg_bin_blur3, { 3, 3 }, 0.0f);
+		cv::GaussianBlur(edge_cur_bin, avg_bin_blur3, { 3, 3 }, 0.0f);
 
+		cv::threshold(avg_bin_blur3, avg_bin_blur3, 0, 255, cv::THRESH_BINARY);
+		cv::threshold(thresh_blur3, thresh_blur3, 0, 255, cv::THRESH_BINARY);
 		cv::bitwise_and(avg_bin_blur3, thresh_blur3, subtitle_mask);
 
-		emit_output(OEIP_STAGE_SUBTITLE_MASK, OEIP_COLSPACE_R8, subtitle_mask);
+		if (_avg_subtitle_mask.empty()) {
+			subtitle_mask.copyTo(_avg_subtitle_mask);
+		}
+
+		average_u8(_avg_subtitle_mask, subtitle_mask, _avg_subtitle_mask);
+
+		emit_output(OEIP_STAGE_SUBTITLE_MASK, OEIP_COLSPACE_R8, _avg_subtitle_mask);
 
 		if (has_output_callback) {
 			for (int i = 0; i < OEIP_STAGE_OUTPUT + 1; i++) {
@@ -190,7 +227,14 @@ protected:
 		return true;
 	}
 
-	cv::UMat get_edge_buffer(cv::Mat const& buf) {
+	void average_u8(cv::Mat const& lhs, cv::Mat const& rhs, cv::Mat& out) {
+		cv::Mat acc(lhs.size(), CV_64F, cv::Scalar(0));
+		accumulate(lhs, acc);
+		accumulate(rhs, acc);
+		acc.convertTo(out, CV_8U, 1 / 2.0);
+	}
+
+	cv::UMat detect_edges(cv::Mat const& buf) {
 		cv::UMat src, blurred, blurred_gray;
 
 		buf.copyTo(src);
@@ -199,8 +243,8 @@ protected:
 
 		cv::UMat grad_x, grad_y, grad;
 		cv::UMat abs_grad_x, abs_grad_y;
-		Sobel(blurred_gray, grad_x, CV_16S, 1, 0);
-		Sobel(blurred_gray, grad_y, CV_16S, 0, 1);
+		Sobel(blurred_gray, grad_x, CV_16U, 1, 0);
+		Sobel(blurred_gray, grad_y, CV_16U, 0, 1);
 		convertScaleAbs(grad_x, abs_grad_x);
 		convertScaleAbs(grad_y, abs_grad_y);
 		addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
@@ -208,10 +252,49 @@ protected:
 		return grad;
 	}
 
+	cv::UMat& prev_edge_buffer() {
+		return _edge_buffers[_edge_buffers_cursor];
+	}
+
+	cv::UMat get_edge_buffer(cv::Mat const& buf) {
+		auto edges = detect_edges(buf);
+		emit_output(OEIP_STAGE_CURRENT_EDGE_BUFFER, OEIP_COLSPACE_R8, edges);
+		return edges;
+		/*
+		edges.convertTo(edges, CV_16U);
+		auto prev = prev_edge_buffer();
+		prev.convertTo(prev, CV_16U);
+
+		cv::UMat e_i = edges + prev;
+		cv::UMat avg = average_edge_buffers(e_i);
+		cv::UMat e_i_2 = e_i - avg;
+
+		return e_i_2;
+		*/
+	}
+
 	template<typename InputArray>
 	void emit_output(oeip_stage stage, oeip_buffer_color_space cs, InputArray mat) {
 		mat.copyTo(_output_buffers[stage]);
 		_output_buffer_formats[stage] = cs;
+	}
+
+	cv::UMat average_edge_buffers() {
+		cv::UMat acc = cv::UMat::zeros(_edge_buffers[0].size(), CV_16U);
+
+		auto temp_idx = (_edge_buffers_cursor + 1) % total_edge_buffers;
+		auto const sum_of_weights = total_edge_buffers * (total_edge_buffers + 1) / 2;
+
+		for (int i = 0; i < total_edge_buffers; i++) {
+			cv::UMat eb;
+            _edge_buffers[i].convertTo(eb, CV_16U);
+
+			// auto w = get_edge_buffer_weight(temp_idx, total_edge_buffers, i);
+			auto w = 1;
+			acc += w * eb;
+		}
+
+		return acc / total_edge_buffers;
 	}
 
 private:
@@ -225,6 +308,8 @@ private:
 
 	cv::Mat _output_buffers[oeip_stage::OEIP_STAGE_OUTPUT + 1];
 	oeip_buffer_color_space _output_buffer_formats[oeip_stage::OEIP_STAGE_OUTPUT + 1];
+
+	cv::UMat _avg_subtitle_mask;
 };
 
 std::unique_ptr<IOEIP> make_oeip(char const* pathToVideo) {
